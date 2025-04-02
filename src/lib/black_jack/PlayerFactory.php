f87c9fa1cd12c1f915acdfb38060131e1368a090
takuya <cdwbh564@yahoo.co.jp>
2025-04-02 23:21:57 +0900

    feat:プレイヤー毎に異なるインスタンスを作成するよう変更
    - プレイヤーの複数化に対応するため、個別のプレイヤーインスタンスを作成
    - プレイヤー毎に追加カード取得ロジックが異なるため、クラスを分離

--- src/lib/black_jack/PlayerFactory.php
+++ src/lib/black_jack/PlayerFactory.php
@@ -4,6 +4,8 @@ namespace BlackJack;
 
 use BlackJack\Dealer;
 use BlackJack\Deck;
+use BlackJack\Player;
+use BlackJack\StrongCpu;
 
 class PlayerFactory
 {
@@ -11,8 +13,20 @@ class PlayerFactory
     // 各プレイヤーのインスタンス作成
     public function __construct(private Dealer $dealer, private Deck $deck, private array $playerNames)
     {
-      foreach ($this->playerNames as $playerName) {
-          $this->players[$playerName] = new Player($dealer, $playerName);
-      }
+        // プレイヤー名とクラス名の配列(これはコンストに渡すこと)
+        $infos = [
+            ['name' => $playerNames[0], 'className' => Player::class],
+            ['name' => $playerNames[1], 'className' => StrongCpu::class],
+            ['name' => $playerNames[2], 'className' => NormalCpu::class]
+        ];
+
+        foreach ($infos as $info) {
+            // 配列の名前とクラス名を変数に代入して、下記に渡す
+            $name = $info['name'];
+            $className = $info['className'];
+            $this->players[$name] = new $className($dealer);
+            // 確かプレイヤーがユーザーのみだった時のコード↓
+            // $this->players[$playerName] = new Player($dealer, $playerName);
+        }
     }
 }
