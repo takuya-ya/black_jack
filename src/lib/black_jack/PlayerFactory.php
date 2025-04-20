d5ad6ab9863b2b5179592f00108677887275c391
takuya <cdwbh564@yahoo.co.jp>
2025-04-20 22:47:03 +0900

    refactor: コメントを整理して可読性を向上、仮クラスを追加してエラーを解消
    - TODO:仮クラスの本実装

--- src/lib/black_jack/PlayerFactory.php
+++ src/lib/black_jack/PlayerFactory.php
@@ -11,10 +11,11 @@ use BlackJack\NormalCpu;
 class PlayerFactory
 {
     public $players = [];
-    // 各プレイヤーのインスタンス作成
+
+    // プレイヤー毎に異なる処理が必要なため、個別のインスタンスを作成
     public function __construct(private Dealer $dealer, private Deck $deck, private array $playerNames)
     {
-        // プレイヤー名とクラス名の配列(これはコンストに渡すこと)
+        // 文字列(例'Player')の場合、名前空間が識別できずエラーになる場合があるため、::class(完全修飾クラス名)を使用
         $infos = [
             ['name' => $playerNames[0], 'className' => Player::class],
             ['name' => $playerNames[1], 'className' => StrongCpu::class],
@@ -22,12 +23,9 @@ class PlayerFactory
         ];
 
         foreach ($infos as $info) {
-            // 配列の名前とクラス名を変数に代入して、下記に渡す
             $name = $info['name'];
             $className = $info['className'];
             $this->players[$name] = new $className($dealer);
-            // 確かプレイヤーがユーザーのみだった時のコード↓
-            // $this->players[$playerName] = new Player($dealer, $playerName);
         }
     }
 }
