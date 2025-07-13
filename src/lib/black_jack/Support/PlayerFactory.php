964f34458e30cf9d78e285043216259c546f1710
takuya <cdwbh564@yahoo.co.jp>
2025-07-13 22:56:40 +0900

    fix: $className 呼び出しに $name 引数を追加して引数不足のエラーを修正

--- src/lib/black_jack/Support/PlayerFactory.php
+++ src/lib/black_jack/Support/PlayerFactory.php
@@ -18,14 +18,14 @@ class PlayerFactory
         // 文字列(例'Player')の場合、名前空間が識別できずエラーになる場合があるため、::class(完全修飾クラス名)を使用
         $infos = [
             ['name' => $playerNames[0], 'className' => Player::class],
-            ['name' => $playerNames[1], 'className' => StrongCpu::class],
-            ['name' => $playerNames[2], 'className' => NormalCpu::class]
+            // ['name' => $playerNames[1], 'className' => StrongCpu::class],
+            // ['name' => $playerNames[2], 'className' => NormalCpu::class]
         ];
 
         foreach ($infos as $info) {
             $name = $info['name'];
             $className = $info['className'];
-            $this->players[$name] = new $className($dealer);
+            $this->players[$name] = new $className($dealer, $name);
         }
     }
 }
